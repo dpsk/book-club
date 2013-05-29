19a7ce8f2844cac7664bbceb30e8edc60d78e340
Mikhail Nikalyukin <idups1k@gmail.com>
2013-05-29 12:11:42 +0300

    callback route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 BookClub::Application.routes.draw do
   resources :books
   resources :sessions, only: [:new, :destroy]
+  match "/auth/google_oauth2/callback", to: "session#create"
   root to: "books#index"
 end
